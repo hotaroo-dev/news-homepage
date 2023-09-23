01ca4571405ec0451ed67f7426d00c6760f5f8f5
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-09-23 13:51:47 +0700

    feat(dep): add fonts and images

--- app.vue
+++ app.vue
@@ -5,4 +5,16 @@
 </template>
 
 <script setup lang="ts">
+import '@fontsource-variable/inter'
+
+useHead({
+  title: 'News Homepage',
+  meta: [
+    {
+      name: 'description',
+      content: 'News homepage layout'
+    }
+  ],
+  link: [{ rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' }]
+})
 </script>
