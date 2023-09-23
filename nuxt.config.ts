2dd65fc1ff9ef3ff0665ec0184f6223ba28d8adb
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-09-23 13:20:54 +0700

    feat(dev): add tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    config: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.25rem',
+          md: '1.5rem',
+          lg: '2rem'
+        }
+      }
+    }
+  },
   devtools: { enabled: true }
 })
